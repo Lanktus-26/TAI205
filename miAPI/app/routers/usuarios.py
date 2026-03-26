bf49294b8e5ab1cbb1af6c48086742e8f0daa4d8
Lank206 <gerardojoshuapina0826@gmail.com>
2026-03-26 17:00:47 -0600

    Practica terminada

--- miAPI/app/routers/usuarios.py
+++ miAPI/app/routers/usuarios.py
@@ -3,34 +3,40 @@ from app.data.database import usuarios
 from app.models.usuarios import crear_usuario
 from app.security.auth import verificar_peticion
 
+from sqlalchemy.orm import Session
+from app.data.db import get_db
+from app.data.usuario import usuario as usuarioDB
+
 routerU= APIRouter(
     prefix="/v1/usuarios",
     tags=['CRUD HTTP']
 )
 
 @routerU.get("/")
-async def consultar():
+async def consultar(db:Session= Depends(get_db)):
+    queryUsuarios= db.query(usuarioDB).all()
+    
     return{
         "status":"200",
-        "total": len(usuarios),
-        "data": usuarios
+        "total": len(queryUsuarios),
+        "data": queryUsuarios
         
     }
 #enpoint modificaco para usar pydantic  
 @routerU.post("/",status_code=status.HTTP_201_CREATED)
-async def crear_usuario(usuario:crear_usuario):
-    for usr in usuarios:
-        if usr["id"] == usuario.id:
-            raise HTTPException(
-                status_code=400,
-                detail="El usuario ya existe"
-            )
-    usuarios.append(usuario)
+async def crear_usuario(usuarioP:crear_usuario, db:Session= Depends(get_db)):
+    
+    usuarioNuevo= usuarioDB(nombre=usuarioP.nombre, edad=usuarioP.edad)
+    db.add(usuarioNuevo)
+    db.commit()
+    db.refresh(usuarioNuevo)
+    
     return{
         "mensaje":"Usuario agregado correctamente",
         "status":"200",
-        "ususario":usuario
+        "ususario":usuarioP
     }
+    
   
 @routerU.put("/{id}",status_code=status.HTTP_200_OK)
 async def actualizar_usuario(id:str, usuario:dict):
